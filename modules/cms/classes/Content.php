60b0c0964771f62a8915556ade0ac278c6ad1735
Peter Vaassens <pvaassens@expandonline.nl>
2016-10-08 18:58:24 +0200

    Fix nesting

--- modules/cms/classes/Content.php
+++ modules/cms/classes/Content.php
@@ -11,6 +11,12 @@ use Markdown;
  */
 class Content extends CmsCompoundObject
 {
+
+    /**
+     * @var null Fix max nesting
+     */
+    protected $maxNesting = null;
+    
     /**
      * @var string The container name associated with the model, eg: pages.
      */
