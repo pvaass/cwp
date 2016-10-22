1aa3da2480068bf97c9ae2509e4a5f824f443502
CWP Dev <dev@cwp.nu>
2016-10-22 21:25:42 +0000

    HOOK: Auto saved theme

--- plugins/pvaass/gitcontent/Plugin.php
+++ plugins/pvaass/gitcontent/Plugin.php
@@ -5,6 +5,7 @@ use Cms\Classes\Layout;
 use Cms\Classes\Page;
 use Cms\Classes\Partial;
 use Doctrine\Common\Util\Debug;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 use System\Classes\PluginBase;
 
@@ -20,10 +21,23 @@ class Plugin extends PluginBase
     const CMS_SAVED_EVENT = 'saved';
     public function boot() {
 //        $process = new Process('git add themes');
-
+//
+//        $process = new Process('git config --global user.email "dev@cwp.nu"');
+//        $process->run();
+//        if (!$process->isSuccessful()) {
+//            throw new ProcessFailedException($process);
+//        }
+//
+//        var_dump($process->getOutput());
+//        die();
         $callable = function($event) {
-            $process = new Process('git add themes && git commit -am "HOOK: Auto saved theme"');
+            $process = new Process('git add themes && git commit -am "HOOK: Auto saved theme"',  null, ['HOME' => '/var/www']);
             $process->run();
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+            var_dump($process->getOutput());
         };
 
         $this->addThemeListener(static::CMS_SAVED_EVENT, Content::class, $callable);
